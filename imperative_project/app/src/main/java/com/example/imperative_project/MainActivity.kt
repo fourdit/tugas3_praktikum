7d65c5736d2ede8cdc68c331a0a2d8338ad47c85
fourdit <aditharun95@gmail.com>
2026-09-10 13:31:54 +0700

    commit unversionerd files

---
+++ imperative_project/app/src/main/java/com/example/imperative_project/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.imperativexmlapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.imperative_project.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvMessage = findViewById<TextView>(R.id.tvMessage)
+        val btnChange = findViewById<Button>(R.id.btnChange)
+
+        btnChange.setOnClickListener {
+            tvMessage.text = "Tombol sudah diklik"
+        }
+    }
+}
